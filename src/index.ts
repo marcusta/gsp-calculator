18f64d9b9e3ee112c57a223748b1584a396bd8b4
Marcus Andersson <marcust@axis.com>
2024-12-28 14:21:46 +0100

    Change port to 3005

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from "./server";
 
-const port = parseInt(process.env.PORT || "3004");
+const port = parseInt(process.env.PORT || "3005");
 
 console.log(`Server is running on port ${port}`);
 
